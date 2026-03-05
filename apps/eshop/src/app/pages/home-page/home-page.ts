d4956acfc0bb99e9982810201b0e7b379ab9bb5b
Pcmf <pedroferreira2005@gmail.com>
2026-03-05 22:59:33 +0000

    Instaling PrimeNG, primeicons, primeflex and some testes

--- apps/eshop/src/app/pages/home-page/home-page.ts
+++ apps/eshop/src/app/pages/home-page/home-page.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-
+import { AccordionModule } from 'primeng/accordion';
 @Component({
   selector: 'app-home-page',
-  imports: [ButtonModule],
+  imports: [ButtonModule, AccordionModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
