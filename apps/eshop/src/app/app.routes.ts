878a2112e41bb7a0ef0af8e6fed7f7251430cc2b
Pcmf <pedroferreira2005@gmail.com>
2026-03-05 10:59:41 +0000

    Home page, header components created

--- apps/eshop/src/app/app.routes.ts
+++ apps/eshop/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {path: '', component: HomePage},
+  {path: 'list', loadComponent: () => import('./pages/product-list/product-list')
+    .then(m => m.ProductList)
+   }
+];
--- apps/eshop/src/app/app.ts
+++ apps/eshop/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { HomePage } from "./pages/home-page/home-page";
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, HomePage],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
