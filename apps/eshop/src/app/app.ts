1425c6d5407885c62a525edde3c832344e0f40c5
Pcmf <pedroferreira2005@gmail.com>
2026-03-05 14:58:04 +0000

    Add Banner to Library UI and use it on eshop app

--- apps/eshop/src/app/app.ts
+++ apps/eshop/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Footer } from './shared/footer/footer';
+import { Banner } from '@org/ui';
 
 @Component({
-  imports: [RouterModule, Header, Footer],
+  imports: [RouterModule, Header, Footer, Banner],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
---
+++ libs/ui/src/lib/banner/banner.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-banner',
+  imports: [],
+  templateUrl: './banner.html',
+  styleUrl: './banner.scss',
+  changeDetection: ChangeDetectionStrategy.Default,
+})
+export class Banner {
+
+}
